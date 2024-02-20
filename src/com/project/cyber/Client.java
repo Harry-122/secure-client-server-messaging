7a26cb2776d8b1edc8484d5378cc16ebfbb3b429
Bhavya Bhatt <46445851+Harry-122@users.noreply.github.com>
2024-02-20 07:09:14 +0000

    Alice bob communication

--- src/com/project/cyber/Client.java
+++ src/com/project/cyber/Client.java
@@ -1,9 +1,10 @@
 package com.project.cyber;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.file.Files;
@@ -11,10 +12,11 @@ import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.Scanner;
 
@@ -23,31 +25,32 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
+import com.project.cyber.Server.MessageContent;
+
 public class Client {
+
+	private static final String SERVER = "server";
+	private static final String PUBLIC_KEY = ".pub";
+	private static final String PRIVATE_KEY = ".prv";
+	private static final String PREPEND_STRING = "gfhk2024:";
+	private static final String HEX_FORMAT = "%02X";
+	private static final String MD5 = "MD5";
+
+	private static String host;
+	private static Integer port;
+	private static String userId;
 
 	public static void main(String[] args) {
 
-		if (args.length != 3) {
-			System.out.println("The length of the provided arguments is incorrect!!!");
-			System.out.println(
-					"Please provide correct arguments that are space-seperated values denoting host, port and userid...");
-			return;
-		}
-
-		String host = args[0];
-		Integer port = Integer.parseInt(args[1]);
-		String userId = args[2];
+		initializeHostPortUserId(args);
 
 		System.out.println("Hello " + userId);
 
 		try (Socket s = new Socket(host, port);
 				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-				DataInputStream dis = new DataInputStream(s.getInputStream());) {
+				ObjectInputStream ois = new ObjectInputStream(s.getInputStream());) {
 
-			String encryptedId = sendMD5UserIdToServer(userId);
-			if (encryptedId != null) dos.writeUTF(encryptedId);
-			String encryptedString = sendMessageUtil();
-			if (encryptedString != null) dos.writeUTF(encryptedString);
+			communicateWithTheServer(ois, dos);
 
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -57,65 +60,159 @@ public class Client {
 
 	}
 
-	private static String sendMD5UserIdToServer(String userId) {
-		MessageDigest md;
+	private static void communicateWithTheServer(ObjectInputStream ois, DataOutputStream dos) {
 		try {
-			md = MessageDigest.getInstance("MD5");
-			byte[] d1 = md.digest(userId.getBytes());
-			StringBuilder sb = new StringBuilder();
-			for (byte input : d1) sb.append(String.format("%02X", input));
-			String s1 = sb.toString();
-			System.out.println(sb.toString());
-			return encryptMessageUtil(sb.toString().getBytes());
+			String hashedId = convertUserIdToHashedMD5Id(userId);
+			if (hashedId != null) {
+				dos.writeUTF(hashedId);
+			}
+
+			receiveUnreadMessages(ois);
+
+			String encryptedString = askUserToSendMessage();
+			if (encryptedString != null) {
+				dos.writeUTF(encryptedString);
+				System.out.println("\nYour message is securely sent to the server...");
+				System.out.println("It will be delivered to the recipient when they log in...\n");
+				System.out.println("Nothing more to do now, therefore logging out!!!\n\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void receiveUnreadMessages(ObjectInputStream ois) {
+		try {
+			Integer messageLength = null;
+			if ((messageLength = ois.readInt()) != null) {
+				System.out.println("There are " + messageLength + " unread message(s) for you...");
+				while (messageLength-- > 0) {
+					MessageContent content = (MessageContent) ois.readObject();
+					System.out.println("Date: " + content.getUnencryptedTimestamp().toString());
+					System.out.println("Message: " + decryptionUtil(userId, content.getEncryptedMessage()));
+					System.out.println();
+				}
+			}
+
+		} catch (EOFException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String askUserToSendMessage() {
+		Scanner sc = new Scanner(System.in);
+		String encryptedString = null;
+
+		System.out.println("Do you wish to send a message to anyone? [y/n]");
+		String yesNo = sc.nextLine();
+
+		if ("yes".startsWith(yesNo.toLowerCase())) {
+
+			System.out.println("Please enter the recipient's user id...");
+			String recipientId = sc.nextLine();
+			System.out.println("Please enter the message that you want to send to this recipient securely...");
+			String message = sc.nextLine();
+
+			encryptedString = encryptionUtil(SERVER, recipientId.concat(message).getBytes());
+
+		} else if ("no".startsWith(yesNo.toLowerCase())) {
+			System.out.println("Nothing more to do now, therefore exiting the program...\n\n");
+		} else {
+			System.err.println("Invalid response provided, please enter either y or n...\n\n");
+			sc.close();
+			return askUserToSendMessage();
+		}
+
+		sc.close();
+		return encryptedString;
+	}
+
+	private static String convertUserIdToHashedMD5Id(String userId) {
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance(MD5);
+			byte[] byteDigest = messageDigest.digest(PREPEND_STRING.concat(userId).getBytes());
+
+			StringBuilder hexBuilder = new StringBuilder();
+			for (byte bd : byteDigest) {
+				hexBuilder.append(String.format(HEX_FORMAT, bd));
+			}
+
+			return hexBuilder.toString();
 		} catch (NoSuchAlgorithmException e) {
 		}
 		return null;
 	}
 
-	private static String sendMessageUtil() {
-		Scanner sc = new Scanner(System.in);
-		String encryptedString = null;
-		System.out.println("Do you wish to send a message to anyone? [y/n]\n");
-		String yesNo = sc.nextLine();
-		if (yesNo.equals("y")) {
-			System.out.println("Please enter the recipient's user id...\n");
-			String recipientId = sc.nextLine();
-//				dos.writeUTF(recipientId);)
-			System.out.println("Please enter the message you want to send to this recipient securely...\n");
-			String message = sc.nextLine();
-			encryptedString = encryptMessageUtil(message.getBytes());
-		} else if (yesNo.equals("n")) {
-			System.out.println("Nothing more to do now, therefore exiting the program...\n\n");
-		} else {
-			System.err.println("Invalid response provided, please enter either y or n...\n\n");
-			sc.close();
-			return sendMessageUtil();
-		}
-		sc.close();
-		return encryptedString;
-	}
-
-	private static String encryptMessageUtil(byte[] bytes) {
-		PublicKey pubKey = null;
+	private static String encryptionUtil(String userId, byte[] byteMessage) {
 		try {
-			File f = new File("server.pub");
-			byte[] keyBytes = Files.readAllBytes(f.toPath());
-			X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(keyBytes);
-			KeyFactory kf = KeyFactory.getInstance("RSA");
-			pubKey = kf.generatePublic(pubSpec);
-		} catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
-		}
+			PublicKey pubKey = getPublicKey(userId);
 
-		byte[] raw = null;
-		try {
 			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 			cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-			raw = cipher.doFinal(bytes);
+			byte[] raw = cipher.doFinal(byteMessage);
+
 			return Base64.getEncoder().encodeToString(raw);
 		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
 				| BadPaddingException e) {
+			e.printStackTrace();
 		}
 		return null;
 	}
 
+	private static String decryptionUtil(String userId, String message) {
+		try {
+			PrivateKey prvKey = getPrivateKey(userId);
+
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.DECRYPT_MODE, prvKey);
+			byte[] stringBytes = cipher.doFinal(Base64.getDecoder().decode(message));
+
+			return new String(stringBytes, "UTF8");
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
+				| BadPaddingException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static PublicKey getPublicKey(String userId) {
+		try {
+			File f = new File(userId.concat(PUBLIC_KEY));
+			byte[] keyBytes = Files.readAllBytes(f.toPath());
+			X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(keyBytes);
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			return kf.generatePublic(pubSpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static PrivateKey getPrivateKey(String userId) {
+		try {
+			File f = new File(userId.concat(PRIVATE_KEY));
+			byte[] keyBytes = Files.readAllBytes(f.toPath());
+			PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			return kf.generatePrivate(prvSpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static void initializeHostPortUserId(String[] args) {
+		if (args.length != 3) {
+			System.out.println("The length of the provided arguments is incorrect!!!");
+			System.out.println(
+					"Please provide correct arguments that are space-seperated values denoting host, port and userid...");
+			return;
+		}
+
+		host = args[0];
+		port = Integer.parseInt(args[1]);
+		userId = args[2];
+	}
 }
--- src/com/project/cyber/Server.java
+++ src/com/project/cyber/Server.java
@@ -1,20 +1,26 @@
 package com.project.cyber;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
 
 import javax.crypto.BadPaddingException;
@@ -24,59 +30,228 @@ import javax.crypto.NoSuchPaddingException;
 
 public class Server {
 
+	private static final String SERVER = "server";
+	private static final String PUBLIC_KEY = ".pub";
+	private static final String PRIVATE_KEY = ".prv";
+	private static final String PREPEND_STRING = "gfhk2024:";
+	private static final String HEX_FORMAT = "%02X";
+	private static final String MD5 = "MD5";
+
+	private static HashMap<String, ArrayList<MessageContent>> userMessages = new HashMap<>();
+	private static Integer port;
+
 	public static void main(String[] args) throws IOException {
-		
+
 		System.out.println(System.getProperty("java.runtime.version"));
 
-		if (args.length != 1) {
-			System.out.println("The length of the provided argument is incorrect!!!");
-			System.out.println("Please provide correct argument that denotes port number for the server to run on...");
-			return;
-		}
-
-		Integer port = Integer.parseInt(args[0]);
-		
-		HashMap<String, String[]> mp = new HashMap<>();
+		initializePortNumber(args);
 
 		try (ServerSocket ss = new ServerSocket(port)) {
-			System.out.println("Waiting incoming connection...");
+			System.out.println("Waiting for an incoming socket connection...");
 
 			while (true) {
 
 				try (Socket s = ss.accept();
-						DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+						ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
 						DataInputStream dis = new DataInputStream(s.getInputStream())) {
 
-					String base64Message = null;
-					try {
-						while ((base64Message = dis.readUTF()) != null) {							
-							File f = new File("server.prv");
-							byte[] keyBytes = Files.readAllBytes(f.toPath());
-							PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
-							KeyFactory kf = KeyFactory.getInstance("RSA");
-							PrivateKey prvKey = kf.generatePrivate(prvSpec);
-
-							Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-							cipher.init(Cipher.DECRYPT_MODE, prvKey);
-							byte[] stringBytes = cipher.doFinal(Base64.getDecoder().decode(base64Message));
-							String result = new String(stringBytes, "UTF8");
-							System.out.println(result);
-						}
-					} catch (EOFException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
-						System.out.println("End of the file reached.");
-					}
+					communicateWithTheClient(dis, oos);
 
 				} catch (IOException e) {
-					// IOException is thrown when the client closes its connection
-					System.err.println("Client closed its connection.");
+					System.err.println("Client closed its connection with the server...");
 				}
 
 			}
 		} catch (IOException e) {
-			System.err.println("Error creating the server socket.");
+			System.err.println("Error creating the server socket...");
 			e.getMessage();
 		}
-
 	}
 
+	private static void communicateWithTheClient(DataInputStream dis, ObjectOutputStream oos) {
+		String hexedId = receiveClientHexedMD5UserId(dis);
+		sendClientUnreadMessages(hexedId, oos);
+		receiveClientEncryptedMessage(dis);
+	}
+
+	private static void receiveClientEncryptedMessage(DataInputStream dis) {
+		String base64Message = null;
+		try {
+			if ((base64Message = dis.readUTF()) != null) {
+				String result = decryptionUtil(SERVER, base64Message);
+				String userId;
+				if (result.startsWith("a")) {
+					result = result.substring(5);
+					userId = "alice";
+				} else {
+					result = result.substring(3);
+					userId = "bob";
+				}
+
+				String hashedId = convertUserIdToHashedMD5Id(userId);
+				MessageContent c = new MessageContent(encryptionUtil(userId, result.getBytes()), new Date());
+
+				ArrayList<MessageContent> content;
+				if (userMessages.containsKey(hashedId)) {
+					content = userMessages.get(hashedId);
+				} else {
+					content = new ArrayList<>();
+				}
+				content.add(c);
+				userMessages.put(hashedId, content);
+			}
+		} catch (EOFException e) {
+			System.out.println("End of the file reached...");
+		} catch (IOException e) {
+			System.out.println("Client closed its connection with the server...");
+		}
+	}
+
+	private static void sendClientUnreadMessages(String hexedId, ObjectOutputStream oos) {
+		try {
+			if (userMessages.containsKey(hexedId) && userMessages.get(hexedId).size() > 0) {
+				ArrayList<MessageContent> messages = userMessages.get(hexedId);
+				System.out.println(
+						"There are " + messages.size() + " unread messages for this client, sending them all..");
+				oos.writeInt(messages.size());
+				for (MessageContent content : messages) {
+					oos.writeObject(content);
+				}
+				userMessages.remove(hexedId);
+			} else {
+				System.out.println("There are no messages that needs to be delivered to the client...");
+				oos.writeInt(0);
+			}
+			oos.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String receiveClientHexedMD5UserId(DataInputStream dis) {
+		String hexedUserId = null;
+		try {
+			if ((hexedUserId = dis.readUTF()) != null) {
+				System.out.println("User " + hexedUserId + " logged in successfully!!!");
+			}
+			return hexedUserId;
+		} catch (IOException e) {
+			System.out.println("Client closed its connection with the server...");
+		}
+		return null;
+	}
+
+	private static String convertUserIdToHashedMD5Id(String userId) {
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance(MD5);
+			byte[] byteDigest = messageDigest.digest(PREPEND_STRING.concat(userId).getBytes());
+
+			StringBuilder hexBuilder = new StringBuilder();
+			for (byte bd : byteDigest) {
+				hexBuilder.append(String.format(HEX_FORMAT, bd));
+			}
+
+			return hexBuilder.toString();
+		} catch (NoSuchAlgorithmException e) {
+		}
+		return null;
+	}
+
+	private static String encryptionUtil(String userId, byte[] byteMessage) {
+		try {
+			PublicKey pubKey = getPublicKey(userId);
+
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+			byte[] raw = cipher.doFinal(byteMessage);
+
+			return Base64.getEncoder().encodeToString(raw);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
+				| BadPaddingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static String decryptionUtil(String userId, String message) {
+		try {
+			PrivateKey prvKey = getPrivateKey(userId);
+
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.DECRYPT_MODE, prvKey);
+			byte[] stringBytes = cipher.doFinal(Base64.getDecoder().decode(message));
+
+			return new String(stringBytes, "UTF8");
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
+				| BadPaddingException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static PublicKey getPublicKey(String userId) {
+		try {
+			File f = new File(userId.concat(PUBLIC_KEY));
+			byte[] keyBytes = Files.readAllBytes(f.toPath());
+			X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(keyBytes);
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			return kf.generatePublic(pubSpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static PrivateKey getPrivateKey(String userId) {
+		try {
+			File f = new File(userId.concat(PRIVATE_KEY));
+			byte[] keyBytes = Files.readAllBytes(f.toPath());
+			PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			return kf.generatePrivate(prvSpec);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static void initializePortNumber(String[] args) {
+		if (args.length != 1) {
+			System.err.println("The length of the provided argument is incorrect!!!");
+			System.out.println("Please provide correct argument that denotes port number for the server to run on...");
+			System.exit(-1);
+		}
+
+		port = Integer.parseInt(args[0]);
+	}
+
+	public static class MessageContent implements Serializable {
+		private static final long serialVersionUID = 9131511945077216480L;
+		private String encryptedMessage;
+		private Date unencryptedTimestamp;
+
+		public MessageContent() {
+		}
+
+		public MessageContent(String message, Date timestamp) {
+			this.encryptedMessage = message;
+			this.unencryptedTimestamp = timestamp;
+		}
+
+		public String getEncryptedMessage() {
+			return encryptedMessage;
+		}
+
+		public void setEncryptedMessage(String encryptedMessage) {
+			this.encryptedMessage = encryptedMessage;
+		}
+
+		public Date getUnencryptedTimestamp() {
+			return unencryptedTimestamp;
+		}
+
+		public void setUnencryptedTimestamp(Date unencryptedTimestamp) {
+			this.unencryptedTimestamp = unencryptedTimestamp;
+		}
+	}
 }
